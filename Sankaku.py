33ead7892774d33d82417c5c271cd7b40c43a054
FireRedz <fireredz69420@gmail.com>
2021-08-14 12:37:52 +0800

    add page_limit and shit

--- Sankaku.py
+++ Sankaku.py
@@ -1,5 +1,4 @@
 import requests
-import mimetypes
 import json
 from pathlib import Path
 
@@ -13,15 +12,24 @@ POST_MIME = "file_type"
 # endregion
 
 class Sankaku:
-    __session: object = requests.Session()
+    __session: requests.Session = None
     posts: list = []
 
 
-    def __init__(self, tags: str, folder: str, custom_url: str, print_fn: callable = None):
+    def __init__(self, tags: str, folder: str, custom_url: str, page_limit: str, print_fn: callable = None):
         self.tags = tags
         self.folder = Path(folder)
         self.custom_url = custom_url
         self.print = print_fn
+        self.posts = []
+        self.__session = requests.Session()
+
+        if not page_limit.isdigit():
+            self.output('[Sankaku] Invalid Page Limit Number, setting it to 1 instead.')
+            self.page_limit = 1
+        else:
+            self.output(f'[Sankaku] Maximum page for query is {page_limit}')
+            self.page_limit = int(page_limit)
 
     @staticmethod
     def __getFileType(url: str): 
@@ -32,7 +40,7 @@ class Sankaku:
 
     def download_post(self, post: dict, folder: str):
         if(post[POST_URL] == None):
-            self.output(f"Can't download: {post}")
+            self.output(f"[Sankaku] Can't download: {post}")
 
         r = self.__session.get(post[POST_URL])
         #open(folder+"\\"+str(post[POST_ID]) + self.__getFileType(post[POST_URL]), 'wb').write(r.content)
@@ -46,9 +54,9 @@ class Sankaku:
         res = self.__session.get(f'https://capi-v2.sankakucomplex.com/pools/{id}?lang=en')
 
         if res.status_code != 200:
-            self.output(f'{id} is not a doujin/book, trying normal image api.')
+            self.output(f'[Sankaku] {id} is not a doujin/book, trying normal image api.')
         else:
-            self.output(f'{id} is a doujin/book, reading data!')
+            self.output(f'[Sankaku] {id} is a doujin/book, reading data!')
 
             data = res.json()
             
@@ -58,21 +66,24 @@ class Sankaku:
         res = self.__session.get(f'https://capi-v2.sankakucomplex.com/posts?lang=en&page=1&limit=1&tags=id_range:{id}')
         
         if res.status_code != 200:
-            return self.output('Failed to get api data even on normal api, prolly an invalid id... Returning!')
+            return self.output('[Sankaku] Failed to get api data even on normal api, prolly an invalid id... Returning!')
 
         res = res.json()
 
         if len(res) == 0:
-            return self.output('API returns nothing, returning...'), None
+            return self.output('[Sankaku] API returns nothing, returning...'), None
 
         return [res[0]], None
 
     def get_posts(self):
-        page = ""
         self.posts = []
-        temp = [0]
+        page = ""
+        temp = []
 
-        while(page != None):
+        for _ in range(self.page_limit):
+            if page == None:
+                break
+
             temp = self._getPage(page)
             page = temp['meta']['next']
             self.posts.extend(temp['data'])
@@ -80,7 +91,7 @@ class Sankaku:
         return self.posts
 
     def _getPage(self, page: int = None):
-        print("G("+self.tags+"):"+ str(page))
+        print(f'G({self.tags}): {page}')
 
         params = {
             'lang':'en',
@@ -91,7 +102,7 @@ class Sankaku:
         if (page != None):
             params['next'] = page
 
-        return json.loads(Sankaku.__session.get(API_URL + 'posts/keyset', params = params).content)
+        return json.loads(self.__session.get(API_URL + 'posts/keyset', params = params).content)
 
     def output(self, string: str):
         if self.print:
@@ -102,28 +113,28 @@ class Sankaku:
 
         # Check if theres custom url or some shit
         if len(self.custom_url) > 0:
-            self.output('Custom ID given, using that instead.')
+            self.output('[Sankaku] Custom ID given, using that instead.')
             posts, folder_name = self.get_info_from_id(self.custom_url)
 
             if folder_name:
-                self.output(f'Saving downloads into {folder_name}')
+                self.output(f'[Sankaku] Saving downloads into {folder_name}')
                 self.folder = self.folder / folder_name
         else:
-            self.output('No custom url given, downloading based off tags.')
+            self.output('[Sankaku] No custom url given, downloading based off tags.')
             self.progress = 0
             posts = self.get_posts()
 
         # GOT DAMN
         if not posts:
-            return self.output('No Posts/Image found, returning.')
+            return self.output('[Sankaku] No Posts/Image found, returning.')
 
         if not self.folder.exists():
-            self.output('Download folder did not exists, creating!')
+            self.output('[Sankaku] Download folder did not exists, creating!')
             self.folder.mkdir()
 
         total = len(posts)
         for i in range(total):
-            self.output(f'D({i+1}/{total}): {posts[i][POST_ID]}')
+            self.output(f'[Sankaku] D({i+1}/{total}): {posts[i][POST_ID]}')
             self.download_post(posts[i],self.folder)
 
-        self.output("Complete")
+        self.output("[Sankaku] Complete")
